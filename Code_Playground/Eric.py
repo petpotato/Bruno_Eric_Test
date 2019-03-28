b6e0b6eb5039eb07b779f8bd302f0b8f479386b8
Bruno <petpotato@gmail.com>
2019-03-27 19:24:11 -0700

    First python test code

---
+++ Code_Playground/Eric.py
@@ -0,0 +1,9 @@
+
+name = input("What's your name? \n")
+
+if name == "Eric" or name =="eric":
+	print("Hello " + name)
+elif name == "Bruno" or name == "bruno":
+	print("God is that you?")		
+else:
+	print("Liar")
