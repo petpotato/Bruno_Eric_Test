e50097bbf3c6954a07c38bdb01b8888d9d9bd1b3
Bruno <petpotato@gmail.com>
2019-03-28 21:14:24 -0700

    break this

--- Code_Playground/Eric.py
+++ Code_Playground/Eric.py
@@ -1,9 +1,16 @@
 
-name = input("What's your name? \n")
-
-if name == "Eric" or name =="eric":
-	print("Hello " + name)
-elif name == "Bruno" or name == "bruno":
-	print("God is that you?")		
-else:
-	print("Liar")
+while True:
+			name = input("What's your name? \n")
+			if name.isdigit():
+				print("Number Error")
+				break
+			elif name == "Bruno" or name == "bruno":
+				print("Hello " + name)
+				break
+			else:
+				print("Huh? ")		
+	
+#elif name == "Bruno" or name == "bruno":
+#	print("God is that you?")		
+#else:
+#	print("Liar")
